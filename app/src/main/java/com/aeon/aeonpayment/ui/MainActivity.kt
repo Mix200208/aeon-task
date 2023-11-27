950111c6257dc729a53874c715ceb11bd8950c6f
Ace <58069279+Mix200208@users.noreply.github.com>
2023-11-27 10:05:08 +0300

    init project and packages

---
+++ app/src/main/java/com/aeon/aeonpayment/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.aeon.aeonpayment.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aeon.aeonpayment.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
